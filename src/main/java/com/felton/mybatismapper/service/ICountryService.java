abd896dbd8cbc35ce2e707b6fb4854c2b8abc314
felton <isliurui@aliyun.com>
2019-12-20 08:21:12 +0800

    添加业务层代码

---
+++ src/main/java/com/felton/mybatismapper/service/ICountryService.java
@@ -0,0 +1,12 @@
+package com.felton.mybatismapper.service;
+
+/**
+ * ICountryService
+ *
+ * @author liurui
+ * @Description: 业务层
+ * @date 2019/12/20
+ */
+public interface ICountryService {
+    int delectByPrimaryKey(Integer id);
+}
---
+++ src/main/java/com/felton/mybatismapper/service/impl/CountryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.felton.mybatismapper.service.impl;
+
+import com.felton.mybatismapper.dao.ICountryDao;
+import com.felton.mybatismapper.service.ICountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * CountryServiceImpl
+ *
+ * @author liurui
+ * @Description:
+ * @date 2019/12/20
+ */
+@Service
+public class CountryServiceImpl implements ICountryService {
+
+    @Autowired
+    private ICountryDao countryDao;
+
+
+    @Override
+    public int delectByPrimaryKey(Integer id) {
+        return 0;
+    }
+}
