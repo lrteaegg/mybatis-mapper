6b73e55b3f52eaf1db4860631e0e1b464ca89383
lrteaegg <tea_egg@outlook.com>
2019-12-24 09:13:02 +0800

    添加logback文件存储路径

--- src/test/java/com/felton/mybatismapper/service/impl/CountryServiceImplTest.java
+++ src/test/java/com/felton/mybatismapper/service/impl/CountryServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.felton.mybatismapper.service.impl;
 
 import com.felton.mybatismapper.service.ICountryService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 class CountryServiceImplTest {
+
     @Autowired
     private ICountryService countryService;
 
@@ -19,5 +22,12 @@ class CountryServiceImplTest {
     void delectByPrimaryKey() {
         Integer result = countryService.delectByPrimaryKey(1);
         System.out.println(result);
+    }
+
+    @Test
+    public void test1() {
+        log.debug("debug test");
+        log.info("info test");
+        log.warn("warn test");
     }
 }
